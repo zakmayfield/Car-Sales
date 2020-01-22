3e42ee37e71bd4d643cd66379dc428440f80813e
Zachary Mayfield <zakmayfield@gmail.com>
2020-01-22 15:54:40 -0500

    created reducer file with initial state, and a reducer function that returns only default right now

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//redux imports
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//import our reducer
+import { reducer } from './reducers'
+
+//creating our redux store
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
